f61df5ada5f829ecceca784f17de551a522bcb72
Nick Igelbrink <zi.aal2000@gmail.com>
2021-03-14 18:37:04 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Reversi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Reversi";
+            Reversi game = new Reversi();
+            Random r = new Random();
+            (int Black, int White, int Draw) = (0, 0, 0);
+            int games = 10000;
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 20; i++)
+            {
+                (int X, int Y) = game.ValidMoves[r.Next(game.ValidMoves.Length)];
+                game.MakeMove(X, Y);
+            }
+            sw.Start();
+            /*Bitmap image = game.CreateImage();
+            image.Save("test.png");
+            image.Dispose();
+            sw.Stop();
+            Console.WriteLine($"Board position:");
+            game.PrintBoard();
+            Console.WriteLine($"Generating, saving and disposing image finished in {sw.Elapsed}s");
+            Console.ReadKey();*/
+            for (int i = 0; i < games; i++)
+            {
+                game.Reset();
+                //Console.Clear();
+                //game.PrintBoard();
+                while (!game.Finished)
+                {
+                    //Console.ReadKey();
+                    (int X, int Y) = game.ValidMoves[r.Next(game.ValidMoves.Length)];
+                    game.MakeMove(X, Y);
+                    //Console.SetCursorPosition(0, 0);
+                    //game.PrintBoard();
+                    /*Bitmap image = game.CreateImage();
+                    image.Save("test.png");
+                    image.Dispose();*/
+                }
+                if (game.Winner == true)
+                    Black++;
+                else if (game.Winner == false)
+                    White++;
+                else
+                    Draw++;
+                //Console.WriteLine($"Winner: {(game.Winner == true ? "Black" : game.Winner == false ? "White" : "Draw")}");
+                //Console.ReadKey();
+            }
+            sw.Stop();
+            Console.WriteLine($"{games} Reversi Random Move Games:\nBlack won {Black} times\nWhite won {White} times\n{Draw} games were drawn\nfinished in {sw.Elapsed}s");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reversi.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Reversi
+{
+    class Reversi
+    {
+        public bool Turn { get; private set; }
+
+        public bool?[,] Board { get { return (bool?[,])board.Clone(); } }
+        bool?[,] board;
+        bool?[,] startingboard = new bool?[,]
+        {
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, false, true, null, null, null },
+            { null, null, null, true, false, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null }
+        };
+        int width, height;
+
+        public (int X, int Y)[] ValidMoves { get { return validmoves.ToArray(); } }
+        readonly List<(int X, int Y)> validmoves;
+
+        public bool Finished { get; private set; }
+
+        public bool? Winner { get; private set; }
+
+        public void SetStartingBoard(bool?[,] Board)
+            => startingboard = (bool?[,])Board.Clone();
+
+        public Reversi()
+        {
+            Finished = false;
+            validmoves = new List<(int X, int Y)>();
+            Reset();
+        }
+
+        public void Reset()
+        {
+            board = (bool?[,])startingboard.Clone();
+            width = board.GetLength(0);
+            height = board.GetLength(1);
+            Turn = true;
+            GetValidMoves();
+            if (validmoves.Count == 0)
+            {
+                Finished = true;
+                DetermineWinner();
+            }
+            else
+                Finished = false;
+        }
+
+        public void MakeMove(int X, int Y)
+        {
+            if (Finished || board[X, Y] != null || !validmoves.Contains((X, Y)))
+                return;
+
+            board[X, Y] = Turn;
+
+            // flip disks
+            foreach ((int X, int Y) direction in Directions)
+            {
+                int x = X + direction.X;
+                int y = Y + direction.Y;
+                if (x >= 0 && x < width && y >= 0 && y < height && board[x, y] == !Turn)
+                {
+                    x += direction.X;
+                    y += direction.Y;
+                    while (x >= 0 && x < width && y >= 0 && y < height)
+                    {
+                        if (board[x, y] == Turn)
+                        {
+                            x -= direction.X;
+                            y -= direction.Y;
+                            while (x != X || y != Y)
+                            {
+                                board[x, y] = Turn;
+                                x -= direction.X;
+                                y -= direction.Y;
+                            }
+                            goto next;
+                        }
+                        else if (board[x, y] == null)
+                            goto next;
+                        x += direction.X;
+                        y += direction.Y;
+                    }
+                }
+                next:;
+            }
+
+            Turn = !Turn;
+            GetValidMoves();
+            if (validmoves.Count == 0)
+            {
+                Finished = true;
+                DetermineWinner();
+            }
+        }
+
+        void DetermineWinner()
+        {
+            int diff = 0;
+            foreach (bool? piece in board)
+                if (piece == true) // black
+                    diff++;
+                else if (piece == false) // white
+                    diff--;
+
+            // diff > 0 -> more black disks
+            // diff < 0 -> more white disks
+            // diff = 0 -> equal amount of black and white disks
+            Winner = diff > 0 ? true : (diff < 0 ? (bool?)false : null);
+        }
+
+        /*
+         * used for the eight directions to check for neighbouring disks
+         * X X X
+         *  \|/
+         * X-O-X
+         *  /|\
+         * X X X
+         */
+        readonly (int X, int Y)[] Directions = new (int X, int Y)[]
+        {
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1)
+        };
+
+        void GetValidMoves()
+        {
+            validmoves.Clear();
+            for (int X = 0; X < width; X++)
+                for (int Y = 0; Y < height; Y++)
+                    if (board[X, Y] == null)
+                    {
+                        foreach ((int x, int y) direction in Directions)
+                        {
+                            int x = X + direction.x;
+                            int y = Y + direction.y;
+                            if (x >= 0 && x < width && y >= 0 && y < height && board[x, y] == !Turn)
+                            {
+                                x += direction.x;
+                                y += direction.y;
+                                while (x >= 0 && x < width && y >= 0 && y < height)
+                                {
+                                    if (board[x, y] == Turn)
+                                    {
+                                        validmoves.Add((X, Y));
+                                        goto valid;
+                                    }
+                                    if (board[x, y] == null)
+                                        break;
+                                    x += direction.x;
+                                    y += direction.y;
+                                }
+                            }
+                        }
+                        valid:;
+                    }
+        }
+
+        public void PrintBoard()
+        {
+            for (int X = 0; X < width; X++)
+            {
+                for (int Y = 0; Y < height; Y++)
+                    Console.Write(board[X, Y] == true ? 'B' : board[X, Y] == false ? 'W' : '_');
+                Console.WriteLine();
+            }
+        }
+
+        const int size = 64;
+        readonly int halfsize = size / 2;
+        readonly Pen p = new Pen(Color.Black, 1);
+        readonly SolidBrush brush = new SolidBrush(Color.Black);
+        readonly Font f = new Font(FontFamily.GenericSansSerif, 22, FontStyle.Bold);
+        readonly Color validmoveb = Color.FromArgb(127, 0, 0, 0), validmovew = Color.FromArgb(127, 255, 255, 255);
+        public Bitmap CreateImage()
+        {
+            int w = width + 1, h = height + 1;
+            Bitmap b = new Bitmap(w * size, h * size);
+            Graphics g = Graphics.FromImage(b);
+            g.Clear(Color.White);
+            brush.Color = Color.FromArgb(135, 199, 255);
+            g.FillRectangle(brush, size, size, w * size, h * size);
+
+            // draw disks
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    if (board[x, y] != null)
+                    {
+                        brush.Color = board[x, y] == true ? Color.Black : Color.White;
+                        g.FillEllipse(brush, (x + 1) * size - 1, (y + 1) * size - 1, size, size);
+                    }
+                    else if (validmoves.Contains((x, y)))
+                    {
+                        brush.Color = Turn ? validmoveb : validmovew;
+                        g.FillEllipse(brush, (x + 1) * size - 1, (y + 1) * size - 1, size, size);
+                    }
+
+            // draw vertical lines and column text
+            brush.Color = Color.Black;
+            int drawx, drawy = h * size;
+            for (int x = 1; x <= w; x++)
+            {
+                drawx = x * size - 1;
+                g.DrawLine(p, drawx, 0, drawx, drawy);
+                if (x == 1)
+                    continue;
+                string text = (x - 1).ToString();
+                SizeF textsize = g.MeasureString(text, f);
+                g.DrawString(text, f, brush, drawx - halfsize - textsize.Width / 2, halfsize - textsize.Height / 2);
+            }
+
+            // draw horizontal lines and row text
+            drawx = w * size;
+            for (int y = 1; y <= h; y++)
+            {
+                drawy = y * size - 1;
+                g.DrawLine(p, 0, drawy, drawx, drawy);
+                if (y == 1)
+                    continue;
+                string text = (y - 1).ToString();
+                SizeF textsize = g.MeasureString(text, f);
+                g.DrawString(text, f, brush, halfsize - textsize.Width / 2, drawy - halfsize - textsize.Height / 2);
+            }
+            g.Dispose();
+            return b;
+        }
+    }
+}
